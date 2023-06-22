b19d72f25c883564507ddb31d1ffba6c12817840
AndreOmniBarry <courageaganwonyi@gmail.com>
2023-06-22 14:21:42 +0100

    update

---
+++ example1.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")({sigint: true});
+const age = prompt ("please insert your age ");
+if (age > 18) {
+    console.log ("you are welcome to the lakers")
+} else {
+    console.log ('go home to your mama')
+}
+// program to print the value of i
+for (let)
